fe0e21950d331d10957911f85e0f141a2898c471
大貫　弘貴 <ohnukihiroki@oonukihiroshitakanoMacBook-Pro.local>
2022-02-19 03:33:52 +0900

    [fix]:read map start

--- srcs/map.c
+++ srcs/map.c
@@ -2,5 +2,19 @@
 
 void	read_map(char *file_name)
 {
-	char	**line;
+	int fd;
+	size_t	i;
+
+	fd = 0;
+	i = 0;
+	fd = open(file_name, O_RDONLY);
+	while (1)
+	{
+		char *line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		else
+			ft_lstnew(line);
+		i++;
+	}
 }
