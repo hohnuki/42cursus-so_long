52df59237cabc18863cd0ee2edec4fd1a0cb3e7b
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-02-25 16:20:15 +0900

    [feat]:freeするファイルを追加

---
+++ srcs/free.c
@@ -0,0 +1,42 @@
+#include "../includes/so_long.h"
+
+char	**free_array(char **map)
+{
+	size_t	i;
+
+	i = 0;
+	while (map[i])
+	{
+		free (map[i]);
+		map[i] = NULL;
+		i++;
+	}
+	free(map);
+	return (NULL);
+}
+
+int		mlx_free(t_config *info)
+{
+	if (info->map_info.map != NULL)
+		info->map_info.map = free_array(info->map_info.map);
+	if (info->images.empty != NULL)
+		mlx_destroy_image(info->mlx_win, info->images.empty);
+	if (info->images.exit != NULL)
+		mlx_destroy_image(info->mlx_win, info->images.exit);
+	if (info->images.collectible != NULL)
+		mlx_destroy_image(info->mlx_win, info->images.collectible);
+	if (info->images.player != NULL)
+		mlx_destroy_image(info->mlx_win, info->images.player);
+	if (info->images.wall != NULL)
+		mlx_destroy_image(info->mlx_win, info->images.wall);
+	if (info->mlx_win != NULL)
+		mlx_destroy_image(info->mlx, info->mlx_win);
+	if (info->mlx != NULL)
+	{
+		mlx_destroy_display(info->mlx);
+		mlx_loop_end(info->mlx);
+		free(info->mlx);
+	}
+	exit (1);
+	return (0);
+}
