61df7a65f9394c0e73056034f4f71297740a1693
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-06 16:45:43 +0900

    [feat]:init_infoをしっかり書きなおした

--- srcs/main.c
+++ srcs/main.c
@@ -2,11 +2,25 @@
 
 void	info_init(t_config *info)
 {
+	info->mlx = NULL;
+	info->mlx_win = NULL;
+	info->map_info.width = 0;
+	info->map_info.height = 0;
+	info->map_info.player_count = 0;
+	info->map_info.exit_count = 0;
+	info->map_info.guard_node = NULL;
+	info->map_info.map = NULL;
+	info->player_info.postion_x = 0;
+	info->player_info.postion_y = 0;
+	info->player_info.pedometer = 0;
+	info->player_info.collectible_count = 0;
+	info->images.player = NULL;
+	info->images.wall = NULL;
+	info->images.collectible = NULL;
+	info->images.exit = NULL;
+	info->images.empty = NULL;
 	info->filename = NULL;
 	info->images.image_size = PIXEL_SIZE;
-	info->map_info.collectible_count = 0;
-	info->player_info.collectible_count = 0;
-	info->player_info.pedometer = 0;
 }
 
 int	main(int argc, char **argv)
