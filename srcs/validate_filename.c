17df9232d4887b7969ee7823b81c5adddbe807e9
大貫　弘貴 <ohnukihiroki8585@yahoo.co.jp>
2022-03-24 09:19:06 +0900

    [fix]:filenameで.berなどのファイル名に対応

--- srcs/validate_filename.c
+++ srcs/validate_filename.c
@@ -5,6 +5,8 @@ void	validate_filename(t_config *info, char *filename)
 	char	*dot_address;
 	int 	strncmp_ret;
 
+	if (ft_strncmp(filename, ".ber", 4) == 0)
+		mlx_free(info);
 	dot_address = ft_strchr(filename, '.');
 	if (dot_address == NULL)
 		mlx_free(info);
